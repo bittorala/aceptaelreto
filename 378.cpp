6d20d74904d68593fd4eb09114765f681299ff07
bittorala <balana@ucm.es>
2020-08-26 17:07:52 +0200

    378

---
+++ 378.cpp
@@ -0,0 +1,63 @@
+// bittor
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+struct tpair{
+	int spent, received;
+	bool operator<(const tpair & other) const
+	{
+		return spent < other.spent ||
+			spent == other.spent && received < other.received;
+	}
+};
+
+inline int minimum(int a, int b)
+{
+	return a < b ? a : b;
+}
+
+inline int maximum(int a, int b)
+{
+	return a < b ? b : a;
+}
+
+void solve(int n)
+{
+	std::vector<tpair> pairs;
+	int spent, received;
+	for (int i = 0; i < n; ++i)
+	{
+		std::cin >> spent;
+		std::cin >> received;
+		pairs.push_back({spent,received});
+	}
+	sort(pairs.begin(), pairs.end());
+	bool fair = true;
+	int j = 1;
+	int i = 0;
+	while (j < n && fair)
+	{
+		while (j < n && pairs[j].spent == pairs[i].spent)
+			++j;
+		if (j == n)
+			break;
+		i = j - 1;
+		if (pairs[i].received >= pairs[j].received)
+			fair = false;
+		++j;
+		++i;
+	}
+	std::cout << (fair ? "SI\n" : "NO\n");
+}
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	int n;
+	for (std::cin >> n; n != 0; std::cin >> n)
+		solve(n);
+	return 0;
+}
